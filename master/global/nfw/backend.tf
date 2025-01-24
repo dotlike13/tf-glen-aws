87ff397238943aa1fe553e35a920e827697a1c11
jdlee01 <jeongdae.lee@airpremia.com>
2025-01-24 17:00:51 +0900

    fix : vpc data remote state

--- master/global/nfw/backend.tf
+++ master/global/nfw/backend.tf
@@ -7,3 +7,25 @@ terraform {
     }
   }
 }
+
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "glen"
+    workspaces = {
+      name = "glen-aws-vpc"
+    }
+  }
+}
+
+data "terraform_remote_state" "tgw" {
+  backend = "remote"
+
+  config = {
+    organization = "glen"
+    workspaces = {
+      name = "glen-aws-tgw"
+    }
+  }
+}
--- master/global/nfw/main.tf
+++ master/global/nfw/main.tf
@@ -2,14 +2,14 @@ module "network_firewall" {
   source = "./module"
 
   name            = "test-nfw"
-  security_vpc_id = module.security_vpc.vpc_id
+  security_vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
   
   prefix = "test"
 
-  firewall_subnet_ids   = module.security_vpc.private_subnet_ids
-  ingress_subnet_ids    = module.security_vpc.gwlbe_ingress_subnet_ids
-  egress_subnet_ids     = module.security_vpc.gwlbe_egress_subnet_ids
-  east_west_subnet_ids  = module.security_vpc.gwlbe_east_west_subnet_ids
+  firewall_subnet_ids   = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  ingress_subnet_ids    = data.terraform_remote_state.vpc.outputs.gwlbe_ingress_subnet_ids
+  egress_subnet_ids     = data.terraform_remote_state.vpc.outputs.gwlbe_egress_subnet_ids
+  east_west_subnet_ids  = data.terraform_remote_state.vpc.outputs.gwlbe_east_west_subnet_ids
 
   tags = merge(local.default_tags, {
     Name = format("%s%s-%s", var.prefix, var.env, var.purpose)
